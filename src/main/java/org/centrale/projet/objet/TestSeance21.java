5db3b70b6317438d47d0d6ab02ef25780d2935d1
Anas Mazouni <madadj4@gmail.com>
2020-09-21 15:47:03 +0200

    add: new character guillome

---
+++ src/main/java/org/centrale/projet/objet/TestSeance21.java
@@ -0,0 +1,22 @@
+package org.centrale.projet.objet;
+
+/**
+ * Test Class for TP2 - Copie d'objet
+ *
+ * @author Groupe 10
+ * @version 0.1
+ */
+public class TestSeance21 {
+  public static void main(String[] args) {
+
+    // Initialize a new game world
+    World game = new World();
+
+    // Display the different characters with a small separator
+    System.out.println("---- \n");
+    game.getRobin().affiche();
+
+    System.out.println("---- \n");
+    game.getGuillomeT().affiche();
+  }
+}
--- src/main/java/org/centrale/projet/objet/World.java
+++ src/main/java/org/centrale/projet/objet/World.java
@@ -15,6 +15,7 @@ public class World {
   private Paysan peon;
   private Lapin bugs;
   private Lapin bunny;
+  private Archer guillaumeT;
 
   // World size (assuming the world map is square)
   private int worldSize;
@@ -30,30 +31,47 @@ public class World {
    */
   public void creeMondeAlea() {
     Random rInt = new Random(); // Random integer generator
+
     // Get a random world position
     Point2D randomPos = new Point2D(rInt.nextInt(this.worldSize), rInt.nextInt(this.worldSize));
 
-    // Place characters around the set random position:
+    /**
+     * Place characters around the set random position:
+     *
+     * - While we could add a while loop to generate random *unique* positions for
+     * each character we went with an easier method, just spreading them around a
+     * random world position (since we need them to be close to each other anyways)
+     *
+     * - If the specs change later on, we could then generate random positions for
+     * each character and store that position to later check if it's not occupied.
+     *
+     * - We had to pass the randomPos variable through a new Point2D constructor
+     * each time instead of passing the actual variable to avoid Java taking in the
+     * reference to the object and assigning the same position value to the 4
+     * characters.
+     *
+     * - The rest of the character stats are randomly set.
+     */
 
-    // - While we could add a while loop to generate random *unique* positions for
-    // each character we went with an easier method, just spreading them around a
-    // random world position (since we need them to be close to each other anyways)
-
-    // - If the specs change later on, we could then generate random positions for
-    // each character and store that position to later check if it's not occupied.
-
-    // - We had to pass the randomPos variable through a new Point2D constructor
-    // each time instead of passing the actual variable to avoid Java taking in the
-    // reference to the object and assigning the same position value to the 4
-    // characters.
-
-    // - The rest of the character stats are randomly set.
-
+    // set Robin
     this.robin = new Archer("Robin", 120, 0, 0, 0, 0, 0, 0, 0, new Point2D(randomPos), 10);
+
+    // Copy Robin to GuillomeT
+    this.guillaumeT = new Archer(this.robin);
+    // Update GuillaumeT's name
+    this.guillaumeT.setNom("GuillomeT");
+    // Move robin
+    this.robin.deplace();
+
+    // set Pion
     randomPos.translate(1, 1);
     this.peon = new Paysan("Peon", 100, 0, 0, 0, 0, 0, 0, 0, new Point2D(randomPos));
+
+    // set Bugs
     randomPos.translate(-2, -2);
     this.bugs = new Lapin(50, 0, 0, 0, new Point2D(randomPos));
+
+    // set Bunny
     randomPos.translate(-2, 0);
     this.bunny = new Lapin(50, 0, 0, 0, new Point2D(randomPos));
   }
@@ -61,6 +79,10 @@ public class World {
   // Helper getters to display the different characters
   public Archer getRobin() {
     return this.robin;
+  }
+
+  public Archer getGuillomeT() {
+    return this.guillaumeT;
   }
 
   public Paysan getPeon() {
@@ -74,4 +96,5 @@ public class World {
   public Lapin getBugs() {
     return this.bugs;
   }
+
 }
